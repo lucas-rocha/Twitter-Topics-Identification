8ee912b56f94e42c1344d6c45b9ff7f619729950
Lucas Rocha <lucas.elias125@gmail.com>
2018-08-23 09:12:02 -0300

    LDA

--- LDA.py
+++ LDA.py
@@ -9,7 +9,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 no_features = 1000
 
-# LDA can only use raw term counts for LDA because it is a probabilistic graphical model
 tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
 tf = tf_vectorizer.fit_transform(documents)
 tf_feature_names = tf_vectorizer.get_feature_names()
@@ -18,19 +17,20 @@ tf_feature_names = tf_vectorizer.get_feature_names()
 
 from sklearn.decomposition import LatentDirichletAllocation
 
-no_topics = 20
-
-# Run LDA
+no_topics = 10
 lda = LatentDirichletAllocation(n_components=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
 
 #------------------------#
-def display_topics(model, feature_names, n_top_words):
+
+def display_topics(model, feature_names, no_top_words):
     for topic_idx, topic in enumerate(model.components_):
         message = "Topic #%d: " % topic_idx
         message += " ".join([feature_names[i]
-        for i in topic.argsort()[:-n_top_words - 1:-1]])
+                            for i in topic.argsort()[:-no_top_words - 1:-1]])
         print(message)
     print()
 
-no_top_words = 10
+#------------------------#
+
+no_top_words = 3
 display_topics(lda, tf_feature_names, no_top_words)
