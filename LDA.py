7bbb90720fc88ae268dfa11f1ea42a4ca3b96625
Lucas Rocha <lucas.elias125@gmail.com>
2018-08-30 10:06:03 -0300

    LDA

--- LDA.py
+++ LDA.py
@@ -1,8 +1,12 @@
-from sklearn.datasets import fetch_20newsgroups
+documents = []
 
-dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
-documents = dataset.data
-
+file = open('Tratando Arquivos/data.txt', 'r')
+i = 0
+for line in file:
+    documents.append(line)
+    i = i + 1
+    print(i)
+file.close()
 #-----------------------#
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
@@ -17,7 +21,7 @@ tf_feature_names = tf_vectorizer.get_feature_names()
 
 from sklearn.decomposition import LatentDirichletAllocation
 
-no_topics = 10
+no_topics = 20
 lda = LatentDirichletAllocation(n_components=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
 
 #------------------------#
@@ -32,5 +36,49 @@ def display_topics(model, feature_names, no_top_words):
 
 #------------------------#
 
-no_top_words = 3
+def display_graph():
+
+
+#------------------------#
+
+def document():
+    # Create Document - Topic Matrix
+    lda_output = best_lda_model.transform(data_vectorized)
+
+    # column names
+    topicnames = ["Topic" + str(i) for i in range(best_lda_model.n_topics)]
+
+    # index names
+    docnames = ["Doc" + str(i) for i in range(len(data))]
+
+    # Make the pandas dataframe
+    df_document_topic = pd.DataFrame(np.round(lda_output, 2), columns=topicnames, index=docnames)
+
+    # Get dominant topic for each document
+    dominant_topic = np.argmax(df_document_topic.values, axis=1)
+    df_document_topic['dominant_topic'] = dominant_topic
+
+    # Styling
+    def color_green(val):
+        color = 'green' if val > .1 else 'black'
+        return 'color: {col}'.format(col=color)
+
+    def make_bold(val):
+        weight = 700 if val > .1 else 400
+        return 'font-weight: {weight}'.format(weight=weight)
+
+    # Apply Style
+    df_document_topics = df_document_topic.head(15).style.applymap(color_green).applymap(make_bold)
+    df_document_topics
+
+    df_topic_distribution = df_document_topic['dominant_topic'].value_counts().reset_index(name="Num Documents")
+    df_topic_distribution.columns = ['Topic Num', 'Num Documents']
+    df_topic_distribution
+    
+#------------------------#
+
+
+#------------------------#
+
+no_top_words = 5
 display_topics(lda, tf_feature_names, no_top_words)
