2ec781cc257b6b9dec924cc47a82df5d953f391f
Lucas Rocha <lucas.elias125@gmail.com>
2019-01-13 14:32:34 -0200

    Matrizes de coocorrencia.

--- Textos para testes/matrizes_coocorrencia_ppmi.py
+++ Textos para testes/matrizes_coocorrencia_ppmi.py
@@ -1,3 +1,23 @@
+import os
+import numpy as np
+import json
+import math
+
+def calculo_ppmi(B, alpha, i, j):
+    Pij = B[i][j]/alpha
+    Pi = math.fsum(B[i])/alpha
+    Pj = math.fsum(B[j]) / alpha
+    K = Pij/(Pi*Pj)
+    if K == 0:
+        return 0
+    P = math.log10(K)
+    if P > 0:
+        return P
+    return 0
+
+#def calculo_alpha(B):
+
+
 def contagem_interseccao(a, b):
     s = set(a)
     return len(s.intersection(b))
@@ -12,12 +32,10 @@ def new_indice(lines):
                 contador += 1
 
 def main():
-    import os
-    import numpy as np
-    import json
     folder = "Documentos Finais/"
-    output_matriz = "Matrizes de Coocorrencia/"
+    matriz_coocorrencia = "Matrizes de Coocorrencia/"
     output_indice = "Indices de Termos/"
+    matriz_PPMI = "Matrizes PPMI/"
     for doc in os.listdir(folder):
         print("--> " + doc)
 
@@ -39,27 +57,42 @@ def main():
                 else:
                     indice_ocorrencia[indice[w]].append(index)
 
-        #Salvando indice Termo-Inteiro
+        # Salvando indice Termo-Inteiro
         file = output_indice + (doc.split('.txt')[0]) + '.json'
         output = open(file, 'w+')
         out = json.dumps(indice)
         output.write(out)
         output.close()
 
-        #Montando e salvando matriz de coocorrencia
-        file = output_matriz + doc
+        # Montando e salvando matriz de coocorrencia
+        file = matriz_coocorrencia + doc
         output = open(file, 'a+')
 
         B = np.zeros((contador,contador), dtype=np.int_)
+        alpha = 0
 
         for i in range(0,contador):
             for j in range(0,contador):
                 B[i][j] = contagem_interseccao(indice_ocorrencia[i], indice_ocorrencia[j])
                 output.write(str(B[i][j]) + ' ')
+                alpha += B[i][j]
             output.write('\n')
 
         output.close()
 
+        # Montando e salvando matriz PPMI
+        file = matriz_PPMI + doc
+        output = open(file, 'a+')
+
+        PPMI = np.zeros((contador, contador), dtype=np.float64)
+
+        for i in range(0,contador):
+            for j in range(0,contador):
+                PPMI[i][j] = calculo_ppmi(B, alpha, i, j)
+                output.write(str(PPMI[i][j]) + ' ')
+            output.write('\n')
+
+        output.close()
 
 # -------------------------------------------------------------------------#
 # Executa o metodo main
